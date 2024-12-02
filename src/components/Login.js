5ef05ea96fa18f7c8321f6ba425db14323fc1c2c
Neeraj <nkumawat34@gmail.com>
2024-12-02 18:48:00 +0530

    Added Loading Screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,83 +1,117 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { MyContext } from '../contextapi/Context_api';
 import { useContext } from 'react';
-import { RotatingLines } from "react-loader-spinner";
+import { RotatingLines } from 'react-loader-spinner';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const backgroundImage = useContext(MyContext);
 
-    const [email,setEmail]=useState('')
-    const[password,setPassword]=useState('')
-    const [loading,setLoading]=useState(false)
-    const navigate=useNavigate()
-    const backgroundImage = useContext(MyContext);
-    const handlesubmit=(e)=>{
-      setLoading(true)
-      e.preventDefault()
-      axios.post('https://budget-tracking-application-backend.onrender.com/api/auth/login', {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    axios
+      .post('https://budget-tracking-application-backend.onrender.com/api/auth/login', {
         email: email,
-        password: password
-    })
-    .then(response => {
-        // Handle the response data here
+        password: password,
+      })
+      .then((response) => {
         console.log(response.data);
-         localStorage.setItem('token'+email,response.data.token)
+        localStorage.setItem('token' + email, response.data.token);
         navigate('/trackingpage', { state: { email } });
-        
-    })
-    .catch(error => {
-        // Handle any errors here
-        alert("user authentication failed")
+      })
+      .catch((error) => {
+        alert('User authentication failed');
         console.error('Error:', error);
-    });
-  setLoading(false)
-    
-    }
+      })
+      .finally(() => {
+        setLoading(false); // Hide the spinner after request completion
+      });
+  };
+
   return (
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" onSubmit={(e)=>handlesubmit(e)} >
-      <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900 float-left">Email address</label>
-        <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={(e)=>setEmail(e.target.value)}/>
-        </div>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 float-left">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-        <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={(e)=>setPassword(e.target.value)}/>
-        </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+
+          <div className="flex flex-row justify-center">
+            <div>
+              Don't have an account{' '}
+              <span className="font-bold mx-2">
+                <button onClick={() => navigate('/signup')}>Register</button>
+              </span>
+            </div>
+          </div>
+        </form>
       </div>
 
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-      <div class='flex flex-row justify-center'>
-        <div>Don't have an account <span class='font-bold mx-2'><button onClick={()=>navigate('/signup')}> Register</button></span></div>
-      </div>
-    </form>
-
-    
-  </div>
-  {loading?<div class='flex justify-center'><RotatingLines
-      strokeColor="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      width="96"
-      visible={true}
-    /></div>:""}
-</div>
-  )
+      {/* Show loading spinner conditionally */}
+      {loading && (
+        <div className="flex justify-center mt-5">
+          <RotatingLines strokeColor="grey" strokeWidth="5" animationDuration="0.75" width="96" visible={true} />
+        </div>
+      )}
+    </div>
+  );
 }
